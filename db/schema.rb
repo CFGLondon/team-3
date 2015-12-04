f62accec61dc3d7716edae0233f926cc67afe6db
Laiq Jafri <laiq.jafri@gmail.com>
2015-12-04 23:23:20 +0000

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204194552) do
+ActiveRecord::Schema.define(version: 20151204224735) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "keyword",    limit: 255
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20151204194552) do
     t.date     "dob"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "email",      limit: 255
+    t.string   "phone",      limit: 255
   end
 
 end
