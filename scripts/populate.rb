a018ab92a6bb402c49bf4e2688a7a9c89c5b1a4b
Laiq Jafri <laiq.jafri@gmail.com>
2015-12-05 01:44:32 +0000

    Script to populate DB with dummy data

---
+++ scripts/populate.rb
@@ -0,0 +1,17 @@
+categories  = ["Education", "Health", "Poverty", "Food", "Unemployment", "Exclusion"]
+whos        = ["Alan", "Youri", "Zichen", "Laiq", "Danny"]
+impairments = ["Visual", "Hearing", "Disabled"]
+locations   = ["United Kingdom", "GB", "London, GB", "Bangladesh", "Sudan", "Cambodia", "Uganda", "Tanzania"]
+
+# WHO Syed Laiq Hussain Jafri WHERE London, GB WHAT Visually Impaired CAT Education
+100.times do |x|
+  who   = whos[rand(5)]
+  where = locations[rand(8)]
+  cat   = categories[rand(6)]
+  what  = impairments[rand(3)]
+  email = SecureRandom.hex(16)
+  phone = rand(10 ** 10)
+  dob   = (Time.now - (365 * rand(17))).strftime("%Y-%m-%d")
+
+  Message.parse "WHO #{who} WHERE #{where} CAT #{cat} WHAT #{what} EMAIL #{email} PHONE #{phone} DOB #{dob}"
+end
