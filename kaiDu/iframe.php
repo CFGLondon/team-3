8c3e482d22c65ff7c0e9f1e3266454b6da928f0f
unknown <kld14@ic.ac.uk>
2015-12-05 00:08:40 +0000

    frontend draft

---
+++ kaiDu/iframe.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>ADD International</title>
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png"/>
+<style type="text/css">body{margin:0;overflow:hidden;}#iframe1{height:100%;left:0px;position:absolute;top:0px;width:100%;}</style>
+</head>
+<body>
+<iframe id="iframe1" src="form.html" frameborder="0"></iframe>
+</body>
+</html>
